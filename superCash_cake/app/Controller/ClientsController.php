9995eec75d9e27aa08ded24ed07b2587524f0733
Daniel <d.hoarau@rt-iut.re>
2013-10-30 22:58:44 +0400

    ajout les fonction de la partie client

--- superCash_cake/app/Controller/ClientsController.php
+++ superCash_cake/app/Controller/ClientsController.php
@@ -2,9 +2,9 @@
 
 
 class ClientsController extends AppController {
-
-	public $uses= array('Client');	
-
+	
+	public $uses= array('factures_client','Client');
+	
 	public function index(){
 		$this->set('Client', $this->Client->find('all'));	
 	}
@@ -17,7 +17,44 @@ class ClientsController extends AppController {
 			$this->Session->setFlash('Erreur');
 		}
 	}
+
+	public function delete_facture($id){
+		if($this->factures_client->delete($id)){
+			$this->Session->setFlash('La facture a ete supprimé.');
+			$this->redirect(array('action' => 'index'));
+		}else{
+			$this->Session->setFlash('Erreur');
+		}
+	}
 	
+	
+	public function detail_client($id){
+		$id=2;
+		if($this->request->is('get')) {
+			$fact = $this->factures_client->find('all',array(
+				'conditions'=>array('factures_client.client_id'=>$id)
+				));
+			$nom = $this->Client->find('first',array(
+				'conditions'=>array('Client.id'=>$id),
+				'fields'=>array('Client.nom')
+				)
+			);
+			$prenom = $this->Client->find('first',array(
+				'conditions'=>array('Client.id'=>$id),
+				'fields'=>array('Client.prenom')
+				)
+			);
+			
+			$nom=$nom['Client']['nom'];
+			$prenom=$prenom['Client']['prenom'];
+			
+			$this->set('Nom',$nom);
+			$this->set('Prenom',$prenom);
+			$this->set('Facture',$fact);
+		} 
+	
+	
+	}
 		
 	
 
